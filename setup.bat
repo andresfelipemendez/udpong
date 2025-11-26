29ba0d38d6f6f7cdc0b801839338c6956e9ab43d
Andrés Felipe Méndez <fmendez1212@gmail.com>
2025-11-26 05:30:00 -0500

    windows build

--- setup.bat
+++ setup.bat
@@ -34,12 +34,18 @@ echo Building SDL3 dependencies...
 echo This may take several minutes on first run.
 echo.
 
-:: Build SDL3
-call :build_dep "%DEPS_DIR%\SDL3" "SDL3"
-call :build_dep "%DEPS_DIR%\SDL3_image" "SDL3_image"
-call :build_dep "%DEPS_DIR%\SDL3_ttf" "SDL3_ttf"
-call :build_dep "%DEPS_DIR%\SDL3_mixer" "SDL3_mixer"
-call :build_dep "%DEPS_DIR%\SDL3_net" "SDL3_net"
+:: Build SDL3 first (base dependency)
+call :build_dep "%DEPS_DIR%\SDL3" "SDL3" ""
+
+:: Get absolute path to SDL3 build for dependent libraries
+for %%I in ("%DEPS_DIR%\SDL3\build") do set SDL3_BUILD_DIR=%%~fI
+
+:: Build SDL3 extension libraries with SDL3_DIR set
+:: Note: AVIF disabled for SDL3_image (requires NASM assembler)
+call :build_dep "%DEPS_DIR%\SDL3_image" "SDL3_image" "-DSDL3_DIR=%SDL3_BUILD_DIR% -DSDLIMAGE_AVIF=OFF"
+call :build_dep "%DEPS_DIR%\SDL3_ttf" "SDL3_ttf" "-DSDL3_DIR=%SDL3_BUILD_DIR%"
+call :build_dep "%DEPS_DIR%\SDL3_mixer" "SDL3_mixer" "-DSDL3_DIR=%SDL3_BUILD_DIR%"
+call :build_dep "%DEPS_DIR%\SDL3_net" "SDL3_net" "-DSDL3_DIR=%SDL3_BUILD_DIR%"
 
 echo.
 echo === Setup Complete ===
@@ -60,6 +66,7 @@ exit /b 0
 :build_dep
 set dep_dir=%~1
 set dep_name=%~2
+set extra_flags=%~3
 
 if not exist "%dep_dir%" (
     echo Warning: %dep_name% not found at %dep_dir%
@@ -73,7 +80,7 @@ if exist "%dep_dir%\build" (
 
 echo.
 echo Building %dep_name%...
-cmake -S "%dep_dir%" -B "%dep_dir%\build" -DCMAKE_BUILD_TYPE=Release
+cmake -S "%dep_dir%" -B "%dep_dir%\build" -DCMAKE_BUILD_TYPE=Release %extra_flags%
 if %ERRORLEVEL% neq 0 (
     echo Error: Failed to configure %dep_name%
     exit /b 1
